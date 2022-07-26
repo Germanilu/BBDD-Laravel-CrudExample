70c95b59ca62454d5a3c8790de4c4f435fc48ff8
Germanilu <lucianogermani93@gmail.com>
2022-07-26 10:36:00 +0200

    Feat: Welcome endpoint

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
 //Contacts
 Route::get('/contacts', function() {
     return "GET contacts";
